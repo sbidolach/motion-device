c3972f3a6dafd9f2ae82fe45d9749f055344ced8
Seweryn Bidolach <sbidolach@op.pl>
2016-06-20 21:54:20 +0100

    dynamic position x

--- js/motion.js
+++ js/motion.js
@@ -25,16 +25,20 @@ if (window.DeviceMotionEvent) {
 
         // Because we don't want to have the device upside down
         // We constrain the x value to the range [-90,90]
-        if (x > 15.5) {
-            x = 15.5
-        }else if (x < -15.5) {
-            x = -15.5
+        var xRightLimit = positionStart.x + 15.5;
+        var xLeftLimit = positionStart.x - 15.5;
+        if (x > xRightLimit) {
+            x = xRightLimit;
+        }else if (x < xLeftLimit) {
+            x = xLeftLimit;
         };
 
-        if (y > 22.5 + positionStart.y) {
-            y = 22.5 + positionStart.y
-        }else if (y < -22.5 + positionStart.y) {
-            y = -22.5 + positionStart.y
+        var xUpLimit = positionStart.y + 22.5;
+        var xDownLimit = positionStart.y - 22.5;
+        if (y > xUpLimit) {
+            y = xUpLimit;
+        }else if (y < xDownLimit) {
+            y = xDownLimit;
         };
 
         output.innerHTML += "x: " + x + "\n";
@@ -47,7 +51,7 @@ if (window.DeviceMotionEvent) {
 
         // 10 is half the size of the ball
         // It center the positioning point to the center of the ball
-        ball.style.left = ((maxX / 2) + x * 6) + "px";
+        ball.style.left = ((maxX / 2) + (x - positionStart.x) * 6) + "px";
         ball.style.top = ((maxY / 2) + ( y - positionStart.y ) * 4) + "px";
 
         output.innerHTML += "top: " + ball.style.top + "\n";
