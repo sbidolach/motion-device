06a848648a001e104b36acb1d1c5d6122658ccc7
Seweryn Bidolach <sbidolach@op.pl>
2016-06-24 17:41:26 +0100

    axis y

--- client/js/motion.js
+++ client/js/motion.js
@@ -4,7 +4,12 @@ var output = document.querySelector('.output');
 
 var maxX = garden.clientWidth - ball.clientWidth;
 var maxY = garden.clientHeight - ball.clientHeight;
-var positionStart = null;
+
+var positionStart;
+
+var yLimit = 22.5;
+var yUpLimit, yDownLimit, yMiddlePoint, yStep;
+
 
 if (window.DeviceMotionEvent) {
     window.addEventListener('deviceorientation', function(eventData) {
@@ -16,11 +21,43 @@ if (window.DeviceMotionEvent) {
         output.innerHTML += "gamma: " + y + "\n";
         //output.innerHTML += "alpha: " + z + "\n";
 
-        if(!positionStart) {
+        if (!positionStart) {
             positionStart = {
                 x: x,
                 y: y
             }
+
+            // get up and down limit bound for axis y and middle point
+            if (!yUpLimit && !yDownLimit) {
+                if (y < 0) {
+                    yUpLimit = y + yLimit;
+                    yDownLimit = y - yLimit;
+                    if (yDownLimit < -90) {
+                        yDownLimit = 90 + (90 + yDownLimit);
+                    }
+                } else if (y >= 0) {
+                    yDownLimit = y - yLimit;
+                    yUpLimit = y + yLimit;
+                    if (yUpLimit > 90) {
+                        yUpLimit = -90 - (90 - yUpLimit);
+                    }
+                }
+
+                var size = null;
+                if (yUpLimit < yDownLimit) {
+                    size = yDownLimit - yUpLimit;
+                } else {
+                    size = 90 + yDownLimit + (90 - yUpLimit)
+                }
+
+                if (y > 0) {
+                    yMiddlePoint = yDownLimit - (size / 2);
+                } else {
+                    yMiddlePoint = yUpLimit + (size / 2);
+                }
+
+            }
+
         }
 
         // Because we don't want to have the device upside down
@@ -29,17 +66,51 @@ if (window.DeviceMotionEvent) {
         var xLeftLimit = positionStart.x - 15.5;
         if (x > xRightLimit) {
             x = xRightLimit;
-        }else if (x < xLeftLimit) {
+        } else if (x < xLeftLimit) {
             x = xLeftLimit;
         };
 
-        var xUpLimit = positionStart.y + 22.5;
-        var xDownLimit = positionStart.y - 22.5;
-        if (y > xUpLimit) {
-            y = xUpLimit;
-        }else if (y < xDownLimit) {
-            y = xDownLimit;
-        };
+        // limita area for axis y
+        if (yDownLimit > yUpLimit) {
+
+            if (y > yUpLimit && y < yMiddlePoint) {
+                y = yUpLimit;
+            } else if (y < yDownLimit && y > yMiddlePoint) {
+                y = yDownLimit;
+            }
+
+            if ((positionStart.y < 0 && y < 0) || (positionStart.y > 0 && y > 0)) {
+                yStep = y - positionStart.y;
+            }
+            // TODO new condition at bottom
+            else {
+                yStep = y - yDownLimit;
+            }
+
+        } else {
+
+            if (yMiddlePoint < 0) {
+                if ((y > yUpLimit && y < 90) || (y > -90 && y < yMiddlePoint)) {
+                    y = yUpLimit;
+                } else if (y < yDownLimit && y > yMiddlePoint) {
+                    y = yDownLimit;
+                }
+            } else {
+                if ((y > yUpLimit && y < yMiddlePoint)) {
+                    y = yUpLimit;
+                } else if ((y < yDownLimit && y > -90) || (y < 90 && y > yMiddlePoint)) {
+                    y = yDownLimit;
+                }
+            }
+
+            yStep = y - positionStart.y;
+
+        }
+
+        output.innerHTML += "yUpLimit: " + yUpLimit + "\n";
+        output.innerHTML += "yDownLimit: " + yDownLimit + "\n";
+        output.innerHTML += "yMiddle: " + yMiddlePoint + "\n";
+        output.innerHTML += "yStep: " + yStep + "\n";
 
         output.innerHTML += "x: " + x + "\n";
         output.innerHTML += "y: " + y + "\n";
@@ -52,7 +123,7 @@ if (window.DeviceMotionEvent) {
         // 10 is half the size of the ball
         // It center the positioning point to the center of the ball
         ball.style.left = ((maxX / 2) + (x - positionStart.x) * 6) + "px";
-        ball.style.top = ((maxY / 2) + ( y - positionStart.y ) * 4) + "px";
+        ball.style.top = ((maxY / 2) + yStep * ((maxY/2)/yLimit)) + "px";
 
         output.innerHTML += "top: " + ball.style.top + "\n";
         output.innerHTML += "left: " + ball.style.left + "\n";
